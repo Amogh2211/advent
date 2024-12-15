b6ba4055324f796cdb8494938e1d0a018f910a87
Amogh2211 <amogh2211@gmail.com>
2024-12-15 00:13:33 -0500

    day 3 solution proper

--- day_3/day_3.cpp
+++ day_3/day_3.cpp
@@ -1,65 +1,69 @@
 // Author: πα
 
 #include <bits/stdc++.h>
+#include <regex>
 
 #define long int64_t
 #define FILENAME "input.txt" 
 
 
-std::vector<std::string> split(char* s, const char* delimiter) {
-    std::vector<std::string> tokens;
-    char* token;
+void solve(std::string& buffer){
+    long res = 0;
 
-    token = strtok(s, delimiter);
-    while (token != NULL) {
-        tokens.push_back(std::string(token));
-        token = strtok(NULL, delimiter);
-    }
+    // finds string of type needed
+
+    // use this for part 1
+    // std::regex mul_regex(R"(mul\([0-9]{1,3},[0-9]{1,3}\))");
+
+    // use this for part 2
+    std::regex mul_regex(R"(mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\))");
     
-    return tokens;
-}
+    
+    auto mul_regex_iter_begin = std::sregex_iterator(buffer.begin(), buffer.end(), mul_regex);
+    auto mul_regex_iter_end = std::sregex_iterator();
 
+    std::regex num_regex(R"(\b[0-9]{1,3}\b)");
+    bool mul_true = true;
+    
+    for (std::sregex_iterator i = mul_regex_iter_begin; i != mul_regex_iter_end; i++) {
+        std::smatch match = *i;
+        std::string s_match = match.str();
+
+        if (s_match == "do()") mul_true = true;
+        if (s_match == "don't()") mul_true = false;
+    
+
+        auto num_regex_iter_begin = std::sregex_iterator(s_match.begin(), s_match.end(), num_regex);
+        auto num_regex_iter_end = std::sregex_iterator();
+        std::vector<int> two_num;
+        for (std::sregex_iterator j = num_regex_iter_begin; j != num_regex_iter_end; j++) {
+            std::smatch num = *j;
+            std::string num_s_match = num.str();
+            two_num.push_back(std::stoi(num_s_match)); 
+        }
+
+        if (two_num.size() == 2 && mul_true) {
+            std::cout << "num1 : " << two_num[0] << " num2: " << two_num[1] << '\n'; 
+            res += two_num[0]*two_num[1]; 
+        }
+    }
+
+    std::cout << "Result = " << res << '\n';
+
+
+}
 
 int main()
 {
     std::ios::sync_with_stdio(false), std::cin.tie(nullptr);
     std::ifstream file(FILENAME);
+    std::stringstream buffer; buffer << file.rdbuf();
 
-    std::vector<std::string> tokens;
+    std::string input = buffer.str();
+
+    // wanted to give input directly as buffer.str() but const says no
+    solve(input);
 
 
-    if (!file.is_open()) {
-        std::cerr << "Error opening the file!";
-        return 1;
-    }
-    
-    std::string s;
-    std::string delimiter = "mul";
-
-    while (getline(file, s)){
-        char* line = new char[s.length() + 1];
-        char* delimiter = "mul";
-
-        std::strcpy(line, s.c_str());
-        tokens = split(line, delimiter);
-        delete[] line;
-    }
-        
-    // Print the tokens stored in the vector
-    for (const auto& t : tokens) {
-        std::cout << t << std::endl;
-    }
-    /////////////////////////////////////////////
-
-    for (int i = 0; i < tokens.size(); ++i){
-        if(tokens[i][0] != '('){
-            continue;
-        }
-        std::string temp;
-        for (int j = 0; j < tokens[i].length(); ++j){
-            
-        }
-    }
-    
     return 0;
 }
